61a029f1b0c92a552581f424db1d77fd85ce4031
mkeychuang <mkey.chuang@gmail.com>
2019-10-13 21:14:50 +0000

    Flixter Lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_enrollment_to_view_lessons, only: [:show]
+	
 	def show
 	end
 	
@@ -7,5 +10,11 @@ class LessonsController < ApplicationController
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
+	end
+	  	
+  	def require_enrollment_to_view_lessons
+	  	if current_user.enrolled_in?(current_lesson.section.course) == false
+		  	redirect_to course_path(current_lesson.section.course), alert: 'NOT CURRENTLY ENROLLED'
+		end
 	end
 end
