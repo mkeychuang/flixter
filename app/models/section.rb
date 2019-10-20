7044d572287e1dd51601b87252d19e5504250476
mkeychuang <mkey.chuang@gmail.com>
2019-10-20 16:00:28 +0000

    Flixter Lesson 37

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,5 +3,10 @@ class Section < ApplicationRecord
 	has_many :lessons
 	
 	include RankedModel
-	ranks :row_order, with_same: :course_id
+	ranks :row_order, :with_same => :course_id
+	
+	def next_section
+		section = course.section.where("row_order > ?", self.row_order).rank(:row_order).first
+		return Section
+	end
 end
